d566e0ea72c275c84a150cad8533891404b6c5f2
JuanBG20 <69605072+JuanBG20@users.noreply.github.com>
2022-10-12 21:11:48 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.goUp').click(function(){
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 0) {
+            $('.goUp').slideDown(300);
+        } else {
+            $('.goUp').slideUp(300);
+        }
+    });
+});
